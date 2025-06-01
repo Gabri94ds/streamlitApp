37d7a52841f49a83e14d25542a3f23a2dce10a49
Gabri94ds <ds.gabri94@gmail.com>
2025-06-01 17:44:11 -0300

    Primeiro commit

---
+++ pages/app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import plotly.express as px
+from dataset import df
+from estudos.Python.streamlit.pages.utils import format_number
+from estudos.Python.streamlit.pages.graficos import grafico_map_estado, grafico_rec_mensal, grafico_rec_estado, grafico_rec_categoria, grafico_rec_vendedores, grafico_vendas_vendedores
+
+st.set_page_config(layout = 'wide')
+
+st.title('Dashboard de vendas :shopping_trolley:')
+
+st.sidebar.title('Filtro de vendedores')
+
+filtro_vendedor = st.sidebar.multiselect(
+    'Vendedores',
+    df['Vendedor'].unique()
+)
+
+if filtro_vendedor:
+    df = df[df['Vendedor'].isin(filtro_vendedor)]
+
+aba1, aba2, aba3 = st.tabs(['Dataset', 'Receita', 'Vendedores'])
+
+with aba1:
+    st.dataframe(df)
+
+with aba2:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric('Receita total', format_number(df['Preço'].sum(),"R$"))
+        st.plotly_chart(grafico_map_estado, use_container_width=True)
+        st.plotly_chart(grafico_rec_estado, use_container_width=True)
+    with col2:
+        st.metric('Quantidade de vendas', df.shape[0])
+        st.plotly_chart(grafico_rec_mensal, use_container_width = True)
+        st.plotly_chart(grafico_rec_categoria, use_container_width = True)
+with aba3:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.plotly_chart(grafico_rec_vendedores, use_container_width = True)
+    
+    with col2:
+        st.plotly_chart(grafico_vendas_vendedores, use_container_width = True)
---
+++ pages/dataset.py
@@ -0,0 +1,17 @@
+
+import json
+import pandas as pd
+
+file = open('vendas.json')
+
+data = json.load(file)
+
+# print(data)
+
+df = pd.DataFrame.from_dict(data)
+
+df['Data da Compra'] = pd.to_datetime(df['Data da Compra'], format = '%d/%m/%Y')
+
+print(df['Data da Compra'])
+
+file.close()
