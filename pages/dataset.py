f1125126d34925dbcf638f023c6abe5f46c8c96a
Gabri94ds <ds.gabri94@gmail.com>
2025-06-03 21:00:19 -0300

    Atualização 03/06 nos arquivos dataframe e utils

--- pages/dataset.py
+++ pages/dataset.py
@@ -12,6 +12,6 @@ df = pd.DataFrame.from_dict(data)
 
 df['Data da Compra'] = pd.to_datetime(df['Data da Compra'], format = '%d/%m/%Y')
 
-print(df['Data da Compra'])
+# print(df['Data da Compra'])
 
 file.close()
